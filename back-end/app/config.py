e765b37634cc29cfe55c4db87410cb13213c0f5a
Adam-Bozek <adamoss12323@gmail.com>
2025-03-12 18:16:55 +0100

    small changes

--- back-end/app/config.py
+++ back-end/app/config.py
@@ -2,7 +2,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'mysecret')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://user:password@postgres_db/db_name')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwtsecret')
