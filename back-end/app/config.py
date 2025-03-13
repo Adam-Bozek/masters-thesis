74bb12d436436b0b83f516251950a042a2c0503a
Adam-Bozek <adamoss12323@gmail.com>
2025-03-13 18:34:14 +0100

    small edits

--- back-end/app/config.py
+++ back-end/app/config.py
@@ -1,7 +1,11 @@
-# app/config.py
 import os
 
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://user:password@postgres_db/db_name')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwtsecret')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
