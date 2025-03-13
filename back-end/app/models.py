fdf5663ecc77fa8cf08e44f6360019a1843b288e
Adam-Bozek <adamoss12323@gmail.com>
2025-03-13 18:40:16 +0100

    added tablename into models.py

--- back-end/app/models.py
+++ back-end/app/models.py
@@ -2,6 +2,8 @@ from . import db
 from datetime import datetime
 
 class User(db.Model):
+    __tablename__ = "users"
+    
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
