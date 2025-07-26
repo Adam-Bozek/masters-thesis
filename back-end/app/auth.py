cca8acec9723364c9d9967064cdd470087308470
Adam Božek <b0zek.adm@gmail.com>
2025-07-26 15:33:27 +0200

    chore: update dependencies in package.json
    - upgraded next from 15.1.6 to 15.4.4
    - downgraded react and react-dom from ^19.0.0 to ^17.0.2

--- back-end/app/auth.py
+++ back-end/app/auth.py
@@ -1,24 +1,44 @@
 from flask import request, jsonify
 from flask_bcrypt import generate_password_hash, check_password_hash
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, create_refresh_token
+from flask_jwt_extended import (
+    create_access_token,
+    jwt_required,
+    get_jwt_identity,
+    create_refresh_token,
+)
+
+from datetime import datetime
 from .models import db, User
 
 
 def register():
     data = request.get_json()
-    username = data.get('username')
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
     email = data.get('email')
     password = data.get('password')
-    
-    if User.query.filter_by(email=email).first():
-        return jsonify({'message': 'User already exists'}), 400
 
-    hashed_password = generate_password_hash(password).decode('utf-8')
-    new_user = User(username=username, email=email, password_hash=hashed_password)
+    # Check if the email is already registered
+    if User.query.filter_by(email=email).first():
+        return jsonify({"message": "User already exists"}), 400
+
+    # Hash the password
+    hashed_password = generate_password_hash(password)
+
+    # Create new user instance
+    new_user = User(
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        password=hashed_password,
+        created_at=datetime.now()
+    )
+
+    # Add to the database
     db.session.add(new_user)
     db.session.commit()
 
-    return jsonify({'message': 'User registered successfully'}), 201
+    return jsonify({"message": "User registered successfully"}), 201
 
 
 def login():
@@ -33,21 +53,19 @@ def login():
     access_token = create_access_token(identity=user.id, expires_delta=False)
     refresh_token = create_refresh_token(identity=user.id)
 
-    return jsonify({
-        "access_token": access_token,
-        "refresh_token": refresh_token
-    }), 200
+    return jsonify({"access_token": access_token, "refresh_token": refresh_token}), 200
 
 
 def verify():
     @jwt_required()
     def protected():
         current_user = get_jwt_identity()
-        return jsonify({'user_id': current_user}), 200
+        return jsonify({"user_id": current_user}), 200
+
     return protected()
 
 
-@jwt_required(refresh=True) 
+@jwt_required(refresh=True)
 def refresh():
     user_id = get_jwt_identity()
     new_access_token = create_access_token(identity=user_id)
