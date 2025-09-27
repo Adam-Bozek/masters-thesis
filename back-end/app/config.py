b96dc609b7804255f08da39146cfface1f0c2188
Adam Božek <b0zek.adm@gmail.com>
2025-09-27 21:44:40 +0200

    feat: add JWT algorithm configuration to the app config

--- back-end/app/config.py
+++ back-end/app/config.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path="../.env")
 
 class Config:
+    JWT_ALGORITHM  = "HS512"
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     
     SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{os.getenv('DB_USER')}:{ os.getenv('DB_PASSWORD')}@database:5432/{os.getenv('DB_NAME')}"
